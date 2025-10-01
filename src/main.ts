e47c563ff2106b16290027cdb8e99842806edae9
KaterinaDud <dudka.katerinaktep@gmail.com>
2025-10-01 18:20:12 +0300

    Stack file

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 	// Enable graceful shutdown
 	app.enableShutdownHooks();
 
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
